4b0a4d08186e984be01e5d43e86e21bd5bf94c26
Philip Dukhov <philip.dukhov@gmail.com>
2023-02-13 13:31:57 +0700

    replace NotNull with Nonnull

--- centrifuge/src/main/java/io/github/centrifugal/centrifuge/Dns.java
+++ centrifuge/src/main/java/io/github/centrifugal/centrifuge/Dns.java
@@ -1,17 +1,17 @@
 package io.github.centrifugal.centrifuge;
 
-import org.jetbrains.annotations.NotNull;
-
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.List;
+
+import javax.annotation.Nonnull;
 
 /**
  * Represents DNS client used to resolve addresses of a host.
  */
 public interface Dns {
 
-    @NotNull
+    @Nonnull
     List<InetAddress> resolve(String host) throws UnknownHostException;
 
 }
